1d03b016ba74b9771a93d1fff0d1bf6b566a16c0
Xavier Cho <mysticfallband@gmail.com>
2014-10-07 12:47:12 +0900

    Remove unnecessary local import

--- src/main/scala/com/greencatsoft/d3/selection/package.scala
+++ src/main/scala/com/greencatsoft/d3/selection/package.scala
@@ -11,9 +11,5 @@ package object selection {
 
   type ElementIterator[A <: Node, B] = js.ThisFunction2[A, B, Int, Any]
 
-  import Global.d3
-  import svg.{ d3 => d3svg }
-  import html.{ d3 => d3html }
-
   implicit def node2selection[A <: Node, B <: Selection[A, B]](node: A)(implicit d3: SelectionSource[A, B]): B = d3.select(node)
 }
