76a3fef10375816c62235677457862b901c3d2fc
mmx900 <mmx900@gmail.com>
2016-02-24 13:48:53 +0900

    Fix invalid method signature

--- src/main/scala/com/greencatsoft/d3/selection/AttributeHolder.scala
+++ src/main/scala/com/greencatsoft/d3/selection/AttributeHolder.scala
@@ -1,7 +1,6 @@
 package com.greencatsoft.d3.selection
 
 import scala.scalajs.js
-import scala.scalajs.js.UndefOr
 
 import org.scalajs.dom.Node
 
@@ -12,5 +11,5 @@ trait AttributeHolder[A <: Node, B <: Selection[A, B]] extends js.Object {
 
   def attr[T](name: String, provider: ElementIterator[A, T]): B = js.native
 
-  def attr(name: String): UndefOr[String] = js.native
+  def attr(name: String): String = js.native
 }
